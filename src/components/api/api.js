0dd0107bbddbeea58e1aac824fee5f7e1ed0f62b
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2024-02-03 16:41:23 +0200

    Create api.js

---
+++ src/components/api/api.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+export const allContactsGet = async () => {
+    const response = await axios.get(`https://64f1e05b0e1e60602d24644f.mockapi.io/contacts`)
+    console.log(response);
+    return response.data
+}
+
+export const addContacts = async (contactData) => {
+    const response = await axios({
+        method: 'POST',
+        url: `https://64f1e05b0e1e60602d24644f.mockapi.io/contacts`,
+        data: contactData
+    })
+    console.log(response);
+    return response.data
+}
+
+export const removeContacts = async (id) => {
+    const response = await axios.delete(`https://64f1e05b0e1e60602d24644f.mockapi.io/contacts/${id}`);
+    return response.data
+}
+
+
