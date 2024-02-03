52323729a12f20f68465331b3f55c460f7ae8b53
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2024-02-03 16:40:35 +0200

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
-export const App = () => {
-  return (
-    <div>
-      <h1>Start proj</h1>
-    </div>
-  );
+import React from 'react';
+import Contactform from './components/contactForm/contactForm';
+import Contactslist from './components/contactList/contactList';
+import ContactFilter from './components/contactFilter/contactFilter';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import {fetchContacts} from './components/redux/createSlice'
+export const App = () =>  {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchContacts())
+  })
+
+
+
+    return (
+      <>
+        <h1 style={{ fontSize: '24px', color: 'blue', textAlign: 'center' }}>
+          Phonebook
+        </h1>
+        <Contactform />
+        <h2>Contacts</h2>
+        <ContactFilter/>
+        <Contactslist/>
+      </>
+    );
+  
 }
